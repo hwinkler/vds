d20ccdc1cc309509d257efab068ffcd1663c0fed
Hugh Winkler <hughw@hughw.net>
2025-06-06 16:57:42 -0500

    Replace icon with bike-racer

--- gatsby-config.mjs
+++ gatsby-config.mjs
@@ -46,7 +46,7 @@ export default {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png' // This path is relative to the root of the site.
+        icon: 'src/images/bike-racer.png' // This path is relative to the root of the site.
       }
     }
   ]
