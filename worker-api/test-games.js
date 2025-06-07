3fc276be30e932a0cdab5375da71f4c2746b927f
Hugh Winkler <hughw@hughw.net>
2025-06-07 09:58:23 -0500

    Remove worker-api directory

--- worker-api/test-games.js
+++
@@ -1,9 +0,0 @@
-// Test the migrated /api/games endpoint
-fetch('http://localhost:8787/api/games')
-  .then(response => response.json())
-  .then(data => {
-    console.log('✅ Games endpoint response:', JSON.stringify(data, null, 2))
-  })
-  .catch(error => {
-    console.log('❌ Error:', error.message)
-  })
